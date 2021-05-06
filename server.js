9aad970e75d67e0477ecaecb98a0ede4fefe6045
zzy <zhouhoushu@126.com>
2021-05-06 12:27:50 +0800

    use http

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const https = require('https')
+const http = require('http')
 const app = require('./app')
 
 const port = process.env.PORT || 3001
 
-const server = https.createServer(app)
+const server = http.createServer(app)
 
 server.listen(port)
